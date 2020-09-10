b7f6e32487b44cac19f0c1534d57c0ab4f4149db
Edgar Bonet <linux@edgar-bonet.org>
2020-09-10 22:58:18 +0200

    Send the Shift key release after the shifted key
    For the sake of symmetry, we release Shift after accompanying key.
    
    It is unclear whether this is useful, though.

--- src/Keyboard.cpp
+++ src/Keyboard.cpp
@@ -271,6 +271,7 @@ size_t Keyboard_::press(uint8_t k)
 size_t Keyboard_::release(uint8_t k) 
 {
 	uint8_t i;
+	bool has_shift = false;
 	if (k >= 136) {			// it's a non-printing key (not a modifier)
 		k = k - 136;
 	} else if (k >= 128) {	// it's a modifier key
@@ -281,8 +282,8 @@ size_t Keyboard_::release(uint8_t k)
 		if (!k) {
 			return 0;
 		}
-		if (k & 0x80) {							// it's a capital letter or other character reached with shift
-			_keyReport.modifiers &= ~(0x02);	// the left shift modifier
+		if (k & 0x80) {		// it's a capital letter or other character reached with shift
+			has_shift = true;
 			k &= 0x7F;
 		}
 	}
@@ -296,6 +297,10 @@ size_t Keyboard_::release(uint8_t k)
 	}
 
 	sendReport(&_keyReport);
+	if (has_shift) {		// release shift after the accompanying key
+		_keyReport.modifiers &= ~(0x02);	// the left shift modifier
+		sendReport(&_keyReport);
+	}
 	return 1;
 }
 
