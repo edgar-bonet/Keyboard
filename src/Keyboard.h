c7c3d0b2bcac52bf43b16e421ebcaa57e099500a
Wrench <dedmause000@gmail.com>
2026-08-01 08:17:15 -0300

    Add Latin American Spanish keyboard layout (#115)
    Co-authored-by: Edgar Bonet <linux@edgar-bonet.org>

--- src/Keyboard.h
+++ src/Keyboard.h
@@ -114,6 +114,7 @@
 extern const uint8_t KeyboardLayout_de_DE[];
 extern const uint8_t KeyboardLayout_en_US[];
 extern const uint8_t KeyboardLayout_es_ES[];
+extern const uint8_t KeyboardLayout_es_LATAM[];
 extern const uint8_t KeyboardLayout_fr_FR[];
 extern const uint8_t KeyboardLayout_it_IT[];
 extern const uint8_t KeyboardLayout_pt_BR[];
